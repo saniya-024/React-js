7749b8fa7e0a36a6e04e7a611c8ddf00ae5a5d66
saniya-024 <saniyayadav0208@gmail.com>
2026-06-22 14:35:38 +0530

    add 3 files here from where import and export done

--- script.js
+++ script.js
@@ -1,3 +1,7 @@
-var h1=document.createElement('h1')
-h1.innerHTML='hlww from js'
-console.log(h1);
+// var h1=document.createElement('h1')
+// h1.innerHTML='hlww from js'
+// document.body.appendChild(h1)
+
+import a from './app'
+
+console.log(a);
