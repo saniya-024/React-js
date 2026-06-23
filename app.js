8260998672d1331e11bc4c35f65d511b256a710e
saniya-024 <saniyayadav0208@gmail.com>
2026-06-23 10:47:21 +0530

    new default

--- app.js
+++ app.js
@@ -1,2 +1,6 @@
-const a=20;
-export default a 
+// const a=20;
+// export default a 
+
+const arr=[10,20,30,40]
+const user='Sarthk'
+export default user
--- script.js
+++ script.js
@@ -2,6 +2,6 @@
 // h1.innerHTML='hlww from js'
 // document.body.appendChild(h1)
 
-import a from './app.js'
+import user from './app.js'
 
-console.log(a);
+console.log(user);
