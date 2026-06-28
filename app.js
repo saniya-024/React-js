dccbc29edf5787874ae2a4e956e46b819e45229d
saniya-024 <saniyayadav0208@gmail.com>
2026-06-28 21:51:01 +0530

    add these all

--- app.js
+++
@@ -1,6 +0,0 @@
-// const a=20;
-// export default a 
-
-const arr=[10,20,30,40]
-const user='Sarthk'
-export default user
--- script.js
+++
@@ -1,7 +0,0 @@
-// var h1=document.createElement('h1')
-// h1.innerHTML='hlww from js'
-// document.body.appendChild(h1)
-
-import user from './app.js'
-
-console.log(user);
