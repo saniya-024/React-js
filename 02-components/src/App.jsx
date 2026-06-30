73abe123174e5bec6e45020953d077859e47923b
saniya-024 <saniyayadav0208@gmail.com>
2026-06-30 11:36:44 +0530

    add components file

---
+++ 02-components/src/App.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import Card from './component/card'
+
+
+const App = () => {
+  const user='harsh'
+  return (
+    <div>
+      <
+        <Card/>
+        
+      </div>
+    
+    
+  )
+}
+
+export default App
---
+++ 02-components/src/component/card.jsx
@@ -0,0 +1,4 @@
+function Card(){
+    return'hi im card'
+}
+export default Card;
