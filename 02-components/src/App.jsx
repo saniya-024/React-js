0be4cae1f237c2724b32cf4ec3cef5f20efab4b7
saniya-024 <saniyayadav0208@gmail.com>
2026-07-01 11:45:06 +0530

    adding these

--- 02-components/src/App.jsx
+++ 02-components/src/App.jsx
@@ -6,18 +6,15 @@ import Navbar from './component/Navbar'
 const App = () => {
   return (
     <div className='parent'>
-    <div className="Card">
-      <h1>Sannaa</h1>
-      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Sequi, ab sit. Culpa, aut dolorem et animi autem similique molestiae nesciunt quo voluptatem obcaecati assumenda deserunt, ea, neque voluptas quaerat consequatur.</p>
-      <button>View Profile</button>
+    
       
-      
-      
+      <Card/>
+      <Navbar/>
     </div>
-    </div>
+  
     
   )
 }
-<Navbar/>
+
 
 export default App
--- 02-components/src/component/card.jsx
+++ 02-components/src/component/card.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
 
-const card = () => {
+const Card = () => {
   return (
-    <div>
-      
-    </div>
+    <div className="Card">
+      <h1>Sannaa</h1>
+      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Sequi, ab sit. Culpa, aut dolorem et animi autem similique molestiae nesciunt quo voluptatem obcaecati assumenda deserunt, ea, neque voluptas quaerat consequatur.</p>
+      <button>View Profile</button>
+      </div>
   )
 }
 
