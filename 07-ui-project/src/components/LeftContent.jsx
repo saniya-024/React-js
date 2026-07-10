fdf7c0700c2145afdac764c7a6ea7fc53631a0ed
saniya-024 <saniyayadav0208@gmail.com>
2026-07-10 10:46:40 +0530

    make left and right side

--- 07-ui-project/src/components/LeftContent.jsx
+++ 07-ui-project/src/components/LeftContent.jsx
@@ -2,8 +2,13 @@ import React from 'react'
 
 const LeftContent = () => {
   return (
-    <div className='h-full w-1/3 bg-blue-600'>
-      
+    <div className='h-full w-1/3 bg-blue-300'>
+      <div>
+        <h3>Prospective <br /><span>customer</span><br/> segmentation</h3>
+      </div>
+      <div className='text-8xl'>
+        <i className="ri-arrow-right-up-line"></i>
+      </div>
     </div>
   )
 }
--- 07-ui-project/src/components/RightContent.jsx
+++ 07-ui-project/src/components/RightContent.jsx
@@ -1,12 +1,10 @@
 import React from 'react'
+import 'remixicon/fonts/remixicon.css'
 
 const RightContent = () => {
   return (
-    <div className='h-full w-3/4 bg-blue-600'>
-      <div></div>
-      <div>
-        <i class="ri-arrow-right-up-line"></i>
-      </div>
+    <div className='h-full w-3/4 bg-blue-200'>
+      
     </div>
   )
 }
