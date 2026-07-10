ab37cb8ba7e58a0aedffbb285bb92208d742a1aa
saniya-024 <saniyayadav0208@gmail.com>
2026-07-10 11:51:43 +0530

    make right section p and arrow

--- 07-ui-project/src/components/RightCard.jsx
+++ 07-ui-project/src/components/RightCard.jsx
@@ -2,8 +2,20 @@ import React from 'react'
 
 const RightCard = () => {
   return (
-    <div className='h-full w-80 rounded-4xl'>
-  <img className='h-full w-full object-cover 'src="https://images.unsplash.com/photo-1573496359142-b8d87734a5a2?q=80&w=1976&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" alt="" />
+    <div className='h-full overflow-hidden relative w-80 rounded-4xl'>
+  <img className='h-full w-full object-cover'src="https://images.unsplash.com/photo-1573496359142-b8d87734a5a2?q=80&w=1976&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" alt="" />
+  
+  <div className='absolute top-0 left-0 h-full w-full bg-amber-300'>
+<h2>1</h2>
+<div>
+    <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Sequi dolor incidunt asperiores, minus amet soluta</p>
+    <div>
+        <button>Satisfied</button>
+        <button><i className="ri-arrow-right-line"></i></button>
+    </div>
+</div>
+  </div>
+  
     </div>
   )
 }
