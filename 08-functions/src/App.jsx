e001b8fea968f910cea16572d99ac1aaaff072e2
saniya-024 <saniyayadav0208@gmail.com>
2026-07-13 21:35:43 +0530

    add the functions of react

--- 08-functions/src/App.jsx
+++ 08-functions/src/App.jsx
@@ -3,13 +3,10 @@ import React from 'react'
 const App = () => {
   function btnClicked(){
     console.log('button is clicked');
-
   }
   return (
     <div>
-    
-
-      <button onClick={btnClicked()}>Click here</button>
+      <button>Click here</button>
     </div>
   )
 }
