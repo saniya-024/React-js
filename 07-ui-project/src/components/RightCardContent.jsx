a3b44d0acdd45fe1b1c6dfca794f92165339954e
saniya-024 <saniyayadav0208@gmail.com>
2026-07-13 12:42:46 +0530

    funtions started

--- 07-ui-project/src/components/RightCardContent.jsx
+++ 07-ui-project/src/components/RightCardContent.jsx
@@ -1,13 +1,16 @@
 import React from 'react'
 
-const RightCardContent = () => {
+const RightCardContent = (props) => {
+  console.log(props.color);
+
+
   return (
     <div className='absolute top-0 left-0 h-full w-full  p-8 flex flex-col justify-between'>
-<h2 className='bg-white text-xl  font-bold rounded-full h-12 w-12 flex justify-center items-center'>1</h2>
+<h2 className='bg-white text-xl  font-bold rounded-full h-12 w-12 flex justify-center items-center'>1 </h2>
 <div>
     <p className='text-xl leading-relaxed text-white mb-14'>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Sequi dolor incidunt asperiores, minus amet soluta</p>
     <div className='flex justify-between'>
-        <button className='bg-blue-600 text-white font-medium px-8 py-2 rounded-full'>Satisfied</button>
+        <button style={{color:props.color}} className='bg-blue-600 text-white font-medium px-8 py-2 rounded-full'>Satisfied</button>
         <button className='bg-blue-600 text-white font-medium px-4 py-3 rounded-full'>
         <i className="ri-arrow-right-line"></i></button>
     </div>
