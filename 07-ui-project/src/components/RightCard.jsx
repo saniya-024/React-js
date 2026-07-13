70eda75a97167e74890d47cfd6ff4af1a1ad7314
saniya-024 <saniyayadav0208@gmail.com>
2026-07-13 12:16:29 +0530

    add the page

--- 07-ui-project/src/components/RightCard.jsx
+++ 07-ui-project/src/components/RightCard.jsx
@@ -1,12 +1,12 @@
 import React from 'react'
 import RightCardContent from './RightCardContent'
 
-const RightCard = () => {
+const RightCard = (props) => {
   return (
     <div className='h-full shrink-0  overflow-hidden relative w-80 rounded-4xl'>
-  <img className='h-full w-full object-cover'src={props.img} alt="" />
+  <img className='h-full w-full object-cover' src={props.img} alt="" />
   
-  <RightCardContent />
+  <RightCardContent id={props.id} tag={props.tag} />
   
     </div>
   )
