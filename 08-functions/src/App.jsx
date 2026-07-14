e533f23958aa114a9322e65060724e37e39a335a
saniya-024 <saniyayadav0208@gmail.com>
2026-07-14 12:40:49 +0530

    add scroller

--- 08-functions/src/App.jsx
+++ 08-functions/src/App.jsx
@@ -1,14 +1,17 @@
 import React from 'react'
 
 const App = () => {
-  function inputChanging(val){
-console.log(val);
+  const pageScrolling=() =>{
+console.log('page scrolling...');
+
   }
-  
+
   
   return (
-    <div>
-<div className='box'></div>
+    <div onWheel={pageScrolling}>
+<div className="page1"></div>
+<div className="page2"></div>
+<div className="page3"></div>
   </div>  
   )
 }
