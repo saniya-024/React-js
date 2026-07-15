0dcd1520f7da7fdabb05915e8c83f4c7cb4b911c
saniya-024 <saniyayadav0208@gmail.com>
2026-07-15 12:26:14 +0530

    make a small project or an ex by using hooks

--- 09-use-state/src/App.jsx
+++ 09-use-state/src/App.jsx
@@ -1,18 +1,12 @@
-import React, { use, useState, useSyncExternalStore } from 'react'
+import React from 'react'
 
 const App = () => {
- const [num, setNum]= useState(20)
- const [username,setUsername] =useState('sana')
-function changeNum(){
-  setNum(100)
-}
   return (
     <div>
-    <h1>value of num is{num}<br />value of user is{username}</h1>
-    <button onClick={changeNum}>Click</button>
+      <button>increse</button>
+      <button>decrease</button>
     </div>
   )
 }
 
 export default App
-
