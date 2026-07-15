cf6499d1481778ff6a9472a45057ee5d9be6172b
saniya-024 <saniyayadav0208@gmail.com>
2026-07-15 10:10:33 +0530

    add functions in react

--- 08-functions/src/App.jsx
+++ 08-functions/src/App.jsx
@@ -2,13 +2,19 @@ import React from 'react'
 
 const App = () => {
   const pageScrolling=() =>{
+    if(elem>0){
 console.log('page scrolling...');
 
+  }else{
+    console.log('ulta scrolling');
   }
 
-  
+}
   return (
-    <div onWheel={pageScrolling}>
+    <div onWheel={(elem)=>{
+
+pageScrolling(elem.deltaY)
+    }}>
 <div className="page1"></div>
 <div className="page2"></div>
 <div className="page3"></div>
