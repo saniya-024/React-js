fe19f90ac42ee73969bf2160b8604752c3ad7444
saniya-024 <saniyayadav0208@gmail.com>
2026-07-15 13:13:21 +0530

    20 and 10ex done by using useState

--- 09-use-state/src/App.jsx
+++ 09-use-state/src/App.jsx
@@ -1,10 +1,20 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 const App = () => {
+  const[num, setNum]= useState(2)
+  function increaseNum(){
+    setNum(20)
+
+  }
+function decreaseNum(){
+  setNum(10)
+
+}
   return (
     <div>
-      <button>increse</button>
-      <button>decrease</button>
+      <h1>{num}</h1>
+      <button onClick={increaseNum}>increse</button>
+      <button onClick={decreaseNum}>decrease</button>
     </div>
   )
 }
