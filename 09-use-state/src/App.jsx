b880d79fb04d5955a5ee2cb5e499789b2f2b120f
saniya-024 <saniyayadav0208@gmail.com>
2026-07-15 11:20:46 +0530

    add program of usestate

--- 09-use-state/src/App.jsx
+++ 09-use-state/src/App.jsx
@@ -1,9 +1,18 @@
 import React from 'react'
 
 const App = () => {
+  let a = 20;
+
+
+  function changeA(){
+    console.log(a);
+    a++
+    console.log(a);
+  }
   return (
     <div>
-      
+    <h1>value of a is {a}</h1>
+    <button onClick={changeA}>Click</button>
     </div>
   )
 }
