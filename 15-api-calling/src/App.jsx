adebdb78d690741fe61a6b9eccfc250e8681c487
saniya-024 <saniyayadav0208@gmail.com>
2026-07-17 12:48:29 +0530

    print hello

--- 15-api-calling/src/App.jsx
+++ 15-api-calling/src/App.jsx
@@ -1,15 +1,21 @@
-import React from 'react'
 import axios from 'axios'
+import {useState} from 'react'
 
 const App = () => {
-  const getData=async ()=> {
+  const [data,setData]=useState([])
+  const getData= async ()=> {
 
   const response= await axios.get('https://picsum.photos/v2/list')
-  console.log(response);
+  setData(response.data);
   }
   return (
     <div>
       <button onClick={getData}>Get Data</button>
+      <div>
+        {data.map(function(elem,idx){
+ return <h3>hello {idx}</h3>
+        })}
+      </div>
     </div>
   )
 }
