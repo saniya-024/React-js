e2fd159122e7868d7acf41edf0fd93f4bc1190ba
saniya-024 <saniyayadav0208@gmail.com>
2026-07-17 11:29:31 +0530

    by calling other also

--- 15-api-calling/src/App.jsx
+++ 15-api-calling/src/App.jsx
@@ -3,9 +3,9 @@ import React from 'react'
 const App = () => {
   const getData=async()=> {
 
- const response= await fetch('https://jsonplaceholder.typicode.com/todos/1')
-
-    console.log(response);
+ const response= await fetch('https://jsonplaceholder.typicode.com/todos/')
+const data= await response.json()
+    console.log(data);
   }
   return (
     <div>
